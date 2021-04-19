38a8c7f8956e605e2648faafaeaa0628b4a8086a
wzulfikar <me@wzulfikar.com>
2021-04-19 10:53:56 +0700

    Add gumroad script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <script
+            src={`${process.env.NEXT_PUBLIC_BASE_URL}/gumroad-dev.js`}
+            data-gumroad-domains="localhost:3000,gumroad.com,gumroad.dev"
+            defer
+          ></script>
         </Head>
         <body className="antialiased sm:subpixel-antialiased md:antialiased">
           <Main />
