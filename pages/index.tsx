ac4ae0c6cc91b625f28a06745905d5655144a3e5
wzulfikar <me@wzulfikar.com>
2021-04-19 18:45:10 +0700

    Add how it works

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,18 @@ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default function Home() {
   return (
-    <div className={`${styles.container} dark:bg-[#111] vercel`}>
-      <h1 className="mt-10 sm:mt-0">Hello Gumroad! Check this out:</h1>
+    <div
+      className={`${styles.container} dark:bg-[#111] vercel bg-gray-200 py-10`}
+    >
+      <h1 className="sm:mt-0">Hello Gumroad! Check this out:</h1>
 
       <div className="mt-10 max-w-sm w-full mx-auto">
         <h1>Gumroad Button Widget</h1>
         <div className="mt-4 grid grid-cols-1 gap-3 text-center">
-          <a className="gumroad-button" href="https://gumroad.com/bite-size-linux">
+          <a
+            className="gumroad-button"
+            href="https://gumroad.com/l/bite-size-linux"
+          >
             Buy my product (gumroad.com)
           </a>
           <a className="gumroad-button" href={`${baseUrl}/bite-size-linux`}>
@@ -31,27 +36,28 @@ export default function Home() {
       <div className="max-w-sm w-full mx-auto mt-10">
         <h1>Product Page</h1>
         <p>
-          You can play around with other products too (no overlay)! Find any
+          You can play around with other products too (no overlay). Find any
           gumroad product page and replace the domain from{' '}
           <code>gumroad.com</code> to <code>gumroad.dev</code>.
         </p>
+
         <p>Example: </p>
         <ul>
           <li>
             - From{' '}
             <a
-              data-overlay="false"
+              data-product="false"
               className="text-blue-800 hover:underline"
               target="_blank"
               rel="noopener noreferrer"
-              href="https://gumroad.com/bite-size-linux"
+              href="https://gumroad.com/l/bite-size-linux"
             >
               gumroad.com/l/bite-size-linux
             </a>{' '}
             to{' '}
             <Link href="/l/bite-size-linux">
               <a
-                data-overlay="false"
+                data-product="false"
                 className="text-blue-800 hover:underline"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -63,18 +69,18 @@ export default function Home() {
           <li>
             - From{' '}
             <a
-              data-overlay="false"
+              data-product="false"
               className="text-blue-800 hover:underline"
               target="_blank"
               rel="noopener noreferrer"
-              href="https://gumroad.com/cefip"
+              href="https://gumroad.com/l/cefip"
             >
               gumroad.com/l/cefip
             </a>{' '}
             to{' '}
             <Link href="/l/cefip">
               <a
-                data-overlay="false"
+                data-product="false"
                 className="text-blue-800 hover:underline"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -85,6 +91,22 @@ export default function Home() {
           </li>
         </ul>
       </div>
+
+      <div className="max-w-sm w-full mx-auto mt-10">
+        <h1>How it works</h1>
+        <p>
+          Behind the scene, the product page uses Next.js ISR to generate the
+          static html on demand (
+          <a
+            className="text-blue-900 hover:underline"
+            href="https://github.com/wzulfikar/gumroad-dev/blob/main/pages/l/[permalink]/index.tsx"
+          >
+            see code in github
+          </a>
+          ). This makes the product pages fast, scalable, and cacheable at edge.
+        </p>
+      </div>
+
       <div className="max-w-sm w-full mx-auto mt-10">
         <h1>API Endpoint</h1>
         <p>
